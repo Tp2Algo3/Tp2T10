e457ce15a9cb044006c728dba1ec294024560423
Franco Batastini <fbatastini@fi.uba.ar>
2020-07-18 21:25:10 -0300

    Sigo modificando respuesta

--- Clases/ClasesPreguntas/Respuesta.java
+++ Clases/ClasesPreguntas/Respuesta.java
@@ -2,6 +2,13 @@ package ClasesPreguntas;
 import java.util.List;
 
 public class Respuesta {
+
+    private String respuesta;
+
+    public Respuesta(String respuesta){
+        this.respuesta = respuesta;
+    }
+
     public boolean esCorrecta(List<Respuesta> respuestas){
         return respuestas.contains(this);
     }
