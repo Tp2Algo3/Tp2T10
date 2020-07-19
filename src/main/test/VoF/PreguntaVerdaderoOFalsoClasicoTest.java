fe9bc408c7087e1e59bea4f1e4708857ddcd2b8a
Franco Batastini <fbatastini@fi.uba.ar>
2020-07-19 12:29:20 -0300

    Primeros test funcionando. Corregidas las dependencias de Maven y de paquetes

--- src/main/test/VoF/PreguntaVerdaderoOFalsoClasicoTest.java
+++ src/main/test/VoF/PreguntaVerdaderoOFalsoClasicoTest.java
@@ -1,29 +1,37 @@
 package VoF;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import ClasesPreguntas.Puntajes.*;
 import ClasesPreguntas.*;
-import org.mockito.InOrder;
 import java.util.List;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
 
 public class PreguntaVerdaderoOFalsoClasicoTest {
 
     private Pregunta VoF;
-    List <Respuesta> respuestas;
+    ArrayList <Respuesta> respuestas;
+
+    @BeforeEach
+    public void setUp(){
+        respuestas = new ArrayList<Respuesta>();
+    }
 
     @Test
     public void test01DadoUnaRespuestaCorrectaSumarPuntaje(){
-        Respuesta respuesta = mock(Respuesta);
+        Respuesta respuesta = mock(Respuesta.class);
         respuestas.add(respuesta);
         when(respuesta.esCorrecta(respuestas)).thenReturn(true);
         VoF = new PreguntaVerdaderoOFalso(respuestas, new PuntajeClasico(), respuestas, "Soy una pregunta");
-        assertEquals(VoF.calcularPuntaje(respuesta), 1);
+        assertEquals(VoF.calcularPuntaje(respuestas), 1);
     }
 
     @Test
     public void test02DadaUnaRespuestaIncorrectaNoSumaPuntaje(){
-        Respuesta respuesta = mock(Respuesta);
+        Respuesta respuesta = mock(Respuesta.class);
         respuestas.add(respuesta);
         when(respuesta.esCorrecta(respuestas)).thenReturn(false);
         VoF = new PreguntaVerdaderoOFalso(respuestas, new PuntajeClasico(), respuestas, "Soy una pregunta");
@@ -33,7 +41,7 @@ public class PreguntaVerdaderoOFalsoClasicoTest {
     @Test
     public void test03SinRespuestaNoSumaPuntaje(){
         respuestas.add(new Respuesta("Soy una respuesta)"));
-        List<Respuesta> listaVacia;
+        List<Respuesta> listaVacia = new ArrayList();
         VoF = new PreguntaVerdaderoOFalso(respuestas, new PuntajeClasico(), respuestas, "Soy una pregunta");
         assertEquals(VoF.calcularPuntaje(listaVacia), 0);
     }
