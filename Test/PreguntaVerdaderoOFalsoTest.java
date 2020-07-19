56540fa3da581acabf1960e3b38dacf39e611708
Franco Batastini <fbatastini@fi.uba.ar>
2020-07-18 21:35:54 -0300

    Modifiqué un poco el test de V/F

--- Test/PreguntaVerdaderoOFalsoTest.java
+++ Test/PreguntaVerdaderoOFalsoTest.java
@@ -1,12 +1,16 @@
 import org.junit.Test;
 import org.mockito.InOrder;
+import ClasesPreguntas.*;
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 
 public class PreguntaVerdaderoOFalsoTest {
     @Test
     public void DadoUnaRespuestaCorrectaSumarPuntaje(){
-    Respuesta respuesta = mock();
+        Respuesta respuesta = mock(Respuesta);
+        when(respuesta.esCorrecta(new List<Respuesta> lista)).thenReturn(true);
+        assertEquals(calcularPuntaje(respuesta), 1);
     }
 
 }
