0a32497f79d94635ab1909693f63908851a9159b
Matias Merlo <mati.merlog@gmail.com>
2020-07-31 17:13:59 -0300

    Modificacion jugador, creacion de GroupChoice y OrderedChoice. Ramiro Sanchez <rsanchez@fi.uba.ar> Franco Batastini <fbatastini@fi.uba.ar> Luciana Hoszowski <lhoszowski@fi.uba.ar>

---
+++ src/main/java/ClasesPreguntas/GroupChoice.java
@@ -0,0 +1,4 @@
+package ClasesPreguntas;
+
+public class GroupChoice {
+}
--- src/main/java/Jugador/Jugador.java
+++ src/main/java/Jugador/Jugador.java
@@ -8,26 +8,41 @@ public class Jugador {
 
     private int puntaje;
     private String nombre;
+    private Potenciador potenciadorActual;
     private ArrayList<Potenciador> potenciadores;
+    private ArrayList<Respuesta> respuestasElegidas;
 
     public Jugador(String nombre){
         this.puntaje = 0;
         this.nombre = nombre;
+        this.potenciadorActual = new MultiplicadorDefecto();
         this.potenciadores = new ArrayList<>();
-        this.potenciadores.add(new MultiplicadorDefecto());
         this.potenciadores.add(new MultiplicadorPorDos());
         this.potenciadores.add(new MultiplicadorPorTres());
         this.potenciadores.add(new ExclusividadPuntaje());
     }
 
-    public ArrayList<Respuesta> responderPregunta(ArrayList<Respuesta> respuestasPosibles){
-        //IMPLEMENTAR LA DEVOLUCIÓN DE LA LISTA DE RESPUESTAS. REQUERIMOS LOS BOTONES DE LA UI
-        //SE RESUELVE CON EVENT HANDLER DE LOS BOTONES.
-        return new ArrayList<>();
+    public ArrayList<Respuesta> responderPregunta(){
+        ArrayList<Respuesta> clonRespuestas = (ArrayList<Respuesta>) respuestasElegidas.clone();
+        respuestasElegidas.clear();
+        return clonRespuestas;
+    }
+
+    public void eliminarRespuesta(Respuesta respuestaARemover){
+        respuestasElegidas.remove(respuestaARemover);
+    }
+
+    public void aniadirRespuesta(Respuesta respuesta){
+        respuestasElegidas.add(respuesta);
     }
 
     public void aumentarPuntaje(int puntajeASumar){
-        this.puntaje += puntajeASumar;
+        this.puntaje += this.potenciadorActual.multiplicarPuntos(puntajeASumar);
+        this.potenciadorActual = new MultiplicadorDefecto();
+    }
+
+    public void utilizarPotenciador(Potenciador potenciadorAUsar){
+        //FALTA IMPLEMENTAR
     }
 
     public int getPuntos(){
