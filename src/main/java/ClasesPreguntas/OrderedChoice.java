2e35f65a543ce271e6ae285de0d7e0d9dd0b752a
Ramiro_Ub_Desk <rsanchez@fi.uba.ar>
2020-08-01 16:16:20 -0300

    Inicio de Orderred Choise y herencia de Pregunta

--- src/main/java/ClasesPreguntas/OrderedChoice.java
+++
@@ -1,4 +0,0 @@
-package ClasesPreguntas;
-
-public class OrderedChoice {
-}
---
+++ src/main/java/ClasesPreguntas/PreguntaOrderedChoice.java
@@ -0,0 +1,25 @@
+package ClasesPreguntas;
+
+import ClasesPreguntas.Puntajes.Puntaje;
+
+import java.util.ArrayList;
+
+public class PreguntaOrderedChoice extends Pregunta{
+
+    public PreguntaOrderedChoice(Puntaje tipoPuntaje, ArrayList<Respuesta> respuestasOrdenadas, String pregunta){
+        super(tipoPuntaje, respuestasOrdenadas, pregunta);
+        if(respuestasPosibles.size() > 5 || respuestasPosibles.size() < 2)
+            throw new RuntimeException("Cantidad de respuestas posibles inválida.");
+    }
+
+    @Override
+    public int calcularPuntaje(ArrayList<Respuesta> respuestasDelUsuario) {
+        boolean estaOrdenada = true;
+
+        while (estaOrdenada){
+
+        }
+
+        return 2;
+    }
+}
