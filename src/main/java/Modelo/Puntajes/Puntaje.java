a72c4e74e2fd94383df79221586ebf1ee373e20a
Franco Batastini <fbatastini@fi.uba.ar>
2020-08-12 19:47:12 -0300

    No contestar en el puntaje devuelve que es erroneo

--- src/main/java/Modelo/Puntajes/Puntaje.java
+++ src/main/java/Modelo/Puntajes/Puntaje.java
@@ -4,6 +4,9 @@ public abstract class Puntaje{
     public abstract int calcularPuntaje(int respCorrectasDelUsuario, int respCorrectasDeLaPregunta, int respuestasTotalesDelUsuario);
 
     public boolean usuarioContestoErroneamente(int respTotalesUsuario, int respCorrectasUsuario){
+        if (respTotalesUsuario == 0){
+            return false;
+        }
         return (respTotalesUsuario>respCorrectasUsuario);
     }
 
